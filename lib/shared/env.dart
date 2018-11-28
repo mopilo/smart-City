588823db1f4447dfd0b26085b2be02c43860cc39
Mopilo <mopahemma@gmail.com>
2018-11-28 11:23:04 +0100

    env variables

---
+++ lib/shared/env.dart
@@ -0,0 +1,29 @@
+
+import 'dart:async';
+import 'package:smart_city/shared/shared_store.dart';
+
+
+class Env {
+  Env();
+
+  String apiUrl= "https://jollypredict.com/api";
+
+  Future<Map> getAuthHeader() async {
+    var token =  await SharedStore().getToken() ;
+    Map<String, String> header = {
+         "Accept": "application/json",
+         "Authorization": "Bearer $token"
+    };
+    return header; 
+  }
+
+   Future<Map> getHeader() async {
+    Map<String, String> header = {
+         "Accept": "application/json",
+    };
+    return header; 
+  }
+
+
+
+}
