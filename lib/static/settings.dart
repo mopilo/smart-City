f6368ed7bd940378ae5099e8f1cf347eaa4e36bc
Mopilo <mopahemma@gmail.com>
2018-12-01 00:25:58 +0100

    UI

--- lib/static/settings.dart
+++ lib/static/settings.dart
@@ -1,19 +1,92 @@
 import 'package:flutter/material.dart';
 
-
-class Settings extends StatelessWidget{
+class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         centerTitle: true,
         backgroundColor: Colors.indigo,
-        title: Text('Settings'),  
+        title: Text(
+          'Settings',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: Center(
-        child: Text('data')
-      ),
+      body: Container(
+          alignment: Alignment.center,
+          // margin: EdgeInsets.only(top: 20),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                onTap: (){print('play');},
+                              child: Container(
+                  margin: EdgeInsets.all( 20 ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Icon(
+                        Icons.person_outline
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Profile'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Divider(),
+              GestureDetector(
+                onTap: (){}, 
+                              child: Padding(
+                  padding: EdgeInsets.all( 20 ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.language
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Language'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Divider(),
+              Padding(
+                padding: EdgeInsets.all( 20 ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.insert_invitation
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Invite Friends'),
+                    )
+                  ],
+                ),
+              ),
+              Divider(),
+              Padding(
+                padding: EdgeInsets.all( 20 ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.info_outline
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('About'),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
-
 }
